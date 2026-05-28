68f9e7a4861f5399d6e5c15b07c0d1c65b4bbccb
abc <rithypiseth69@gmail.com>
2026-05-28 20:49:52 +0700

    hi

--- EX-1.dart
+++ EX-1.dart
@@ -1,61 +1,96 @@
-enum Skill { Flutter , Dart , Other}
+enum Skill { Flutter, Dart, Other }
 
 class Address {
   final String _street;
   final String _city;
   final String _zipCode;
 
-  Address({
-    required final street,
-    required final city,
-    required final zipcode,
-  }) : _street = street, _zipCode = zipcode, _city = city;
+  Address({required final street, required final city, required final zipcode})
+    : _street = street,
+      _zipCode = zipcode,
+      _city = city;
 
   String get street => _street;
   String get city => _city;
   String get zipCode => _zipCode;
-
 }
 
-class Employee{
+class Employee {
   final String _name;
   final int _yearOfExperience;
   final List<Skill> _skills;
   double _salary;
   final Address _address;
 
-  Employee ({
+  Employee({
     required String name,
     required int yearOfExperience,
     required List<Skill> skills,
     required double salary,
     required Address address,
-  }) : _name = name, _yearOfExperience=yearOfExperience, _skills = skills, _salary=salary, _address=address;
+  }) : _name = name,
+       _yearOfExperience = yearOfExperience,
+       _skills = skills,
+       _salary = salary,
+       _address = address;
+
+  // Named Constructor
+
+  Employee.mobileDeveloper({
+    required String name,
+    required int yearOfExperience,
+    required double salary,
+    required Address address,
+    List<Skill> skill = const [Skill.Flutter, Skill.Dart],
+  }) : _name = name,
+       _yearOfExperience = yearOfExperience,
+       _salary = salary,
+       _address = address,
+       _skills = skill;
 
   int get experience => _yearOfExperience;
-  
+
   String get name => _name;
   List get skill => _skills;
   Address get address => _address;
 
   double get salary {
-    if (_skills == Skill.Flutter){
+    if (_skills == Skill.Flutter) {
       _salary += 1500;
     }
-    if (_skills == Skill.Dart){
+    if (_skills == Skill.Dart) {
       _salary += 1200;
     }
-    if(_skills == Skill.Other){
+    if (_skills == Skill.Other) {
       _salary += 800;
     }
-    return _salary * (_yearOfExperience/2) ;
+    return _salary * (_yearOfExperience / 2);
   }
 
+  void printDetails() {
+    print("Salary Specification : ");
+    print("\t Base Salary : ${salary}\$");
+    print("\t Experience : ${_yearOfExperience}y");
+    print("\t Skill : ${_skills}");
+    print(
+      "\t Address : ${address._city}, ${address.street}, ${address._zipCode} ",
+    );
+  }
 }
 
-void main(){
- Employee e1 = Employee(name: "Seth", yearOfExperience: 3, skills: [Skill.Flutter], salary: 5000, address: Address(city: "Phnom Penh", street: "67", zipcode: "007"));
- print(e1.name);
- print(e1.address.city);
- print(e1.salary);
+void main() {
+  //  Employee e1 = Employee(name: "Seth", yearOfExperience: 3, skills: [Skill.Flutter],
+  //                salary: 5000, address: Address(city: "Phnom Penh", street: "67", zipcode: "007"));
+  //  print(e1.name);
+  //  print(e1.address.city);
+  //  print(e1.salary);
+  //  e1.printDetails();
+
+  Employee e2 = Employee.mobileDeveloper(
+    name: "Yanit",
+    yearOfExperience: 5,
+    salary: 8000,
+    address: Address(street: "110", city: "Phnom Penh", zipcode: "168"),
+  );
+  e2.printDetails();
 }
--- EX-2.dart
+++ EX-2.dart
@@ -1,76 +1,67 @@
-import 'dart:io';
-
 class BankAccount {
-      int id;
-      String name;
-      double amount = 0;
+  int id;
+  String name;
+  double amount = 0;
 
-      BankAccount(this.id,this.name);
-      String get balance => "You have ${amount}\$ in your account";
+  BankAccount(this.id, this.name);
+  String get balance => "You have ${amount}\$ in your account";
 
-      void withdraw(double withdrawn){
-        if (amount <= 0 || amount > withdrawn){
-          print("You dont have enough money to withdrawn.");
-        }else if(withdrawn < 0 ){
-          print("Wrong input");
-        }
-        else{
-          amount -= withdrawn;
-        }
-      }
-
-      void credit(double dakLuy){
-        if(dakLuy <= 0){
-          print("Wrong input");
-        }else{
-          amount+=dakLuy;
-        }
-      }
-      
-  }
-
-  class Bank {
-      // Store user's ID and name
-      String name;
-      Map<int, String> _accounts = {};
-
-      Bank({required this.name});
-      Map<int,String> get account => _accounts;
-
-      // BankAccount createAccount(int accountID, String accountOwner){
-      //   if(_accounts.containsKey(accountID)){
-      //     throw Exception("Invalid ID get a unique one.");
-      //   }else{
-      //     _accounts[accountID] = accountOwner;
-      //   }
-
-      //   return BankAccount(accountID, accountOwner);
-      // }
-      
-  }
-
-
-  
-  void main() {
-
-    Bank myBank = Bank(name: "CADT Bank");
-    BankAccount ronanAccount = myBank.createAccount(100, 'Ronan');
-
-    print(ronanAccount.balance); // Balance: $0
-    ronanAccount.credit(100);
-    print(ronanAccount.balance); // Balance: $100
-    ronanAccount.withdraw(50);
-    print(ronanAccount.balance); // Balance: $50
-
-    try {
-      ronanAccount.withdraw(75); // This will throw an exception
-    } catch (e) {
-      print(e); // Output: Insufficient balance for withdrawal!
-    }
-
-    try {
-      myBank.createAccount(100, 'Honlgy'); // This will throw an exception
-    } catch (e) {
-      print(e); // Output: Account with ID 100 already exists!
+  void withdraw(double withdrawn) {
+    if (amount <= 0 || amount > withdrawn) {
+      print("You dont have enough money to withdrawn.");
+    } else if (withdrawn < 0) {
+      print("Wrong input");
+    } else {
+      amount -= withdrawn;
     }
   }
+
+  void credit(double dakLuy) {
+    if (dakLuy <= 0) {
+      print("Wrong input");
+    } else {
+      amount += dakLuy;
+    }
+  }
+}
+
+class Bank {
+  // Store user's ID and name
+  String name;
+  Map<int, String> _accounts = {};
+
+  Bank({required this.name});
+  Map<int, String> get account => _accounts;
+
+  BankAccount createAccount(int accountID, String accountOwner) {
+    if (_accounts.containsKey(accountID)) {
+      throw Exception("Invalid ID get a unique one.");
+    } else {
+      _accounts[accountID] = accountOwner;
+    }
+    return BankAccount(accountID, accountOwner);
+  }
+}
+
+void main() {
+  Bank myBank = Bank(name: "CADT Bank");
+  BankAccount ronanAccount = myBank.createAccount(100, 'Ronan');
+
+  print(ronanAccount.balance); // Balance: $0
+  ronanAccount.credit(100);
+  print(ronanAccount.balance); // Balance: $100
+  ronanAccount.withdraw(50);
+  print(ronanAccount.balance); // Balance: $50
+
+  try {
+    ronanAccount.withdraw(75); // This will throw an exception
+  } catch (e) {
+    print(e); // Output: Insufficient balance for withdrawal!
+  }
+
+  try {
+    myBank.createAccount(100, 'Honlgy'); // This will throw an exception
+  } catch (e) {
+    print(e); // Output: Account with ID 100 already exists!
+  }
+}
