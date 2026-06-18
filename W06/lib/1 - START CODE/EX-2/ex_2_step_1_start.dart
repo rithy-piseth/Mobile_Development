8bcbddca5f9000de6773a18a9e3188b8169d0e2d
abc <rithypiseth69@gmail.com>
2026-06-18 21:16:20 +0700

    hi

--- W06/lib/1 - START CODE/EX-2/ex_2_step_1_start.dart
+++ W06/lib/1 - START CODE/EX-2/ex_2_step_1_start.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text("Favorite cards"),
-        ),
-        body: Column(
-          children: [
-            Container(
-                // YOUR CODE
-                )
-          ],
-        ),
+void main(){
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text("Favorite Card",style: TextStyle(fontSize: 22),),backgroundColor: Colors.blue,),
+      body: Column(
+        children: [
+          Favorite()
+        ],
       ),
-    ));
+    ),
+  ));
+}
+
+class Favorite extends StatefulWidget{
+  const Favorite({super.key});
+
+  @override
+  State<Favorite> createState() => _Favorite() ;
+}
+
+class _Favorite extends State<Favorite>{
+  bool like = false;
+  void isFavorite(){
+    setState(() {
+      like =!like;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    IconData ic = Icons.favorite_border ;
+    Color? c;
+
+    if(like == true){
+      ic = Icons.favorite;
+      c  = Colors.red;
+    }
+
+    return Container(height: 100, 
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(width: 1, color: Colors.grey)),
+      ),
+          child: Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(child:Text("Title",style:TextStyle(color: Colors.blue,fontSize: 20),) ,),
+                  Expanded(child:Text("Description")) 
+                 ],
+              ),
+              Spacer(),
+              IconButton(onPressed: isFavorite, icon: Icon(ic),color: c,),
+            ],
+          ),
+          );
+  }
+}
